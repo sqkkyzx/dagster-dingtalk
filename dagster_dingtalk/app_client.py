20f1964e8c2e7cfdab6e3989ae2ff61a9d3551b2
YiZixuan <sqkkyzx@qq.com>
2024-11-12 17:17:18 +0800

    优化注释、Docstring、和日志输出。

--- dagster_dingtalk/app_client.py
+++ dagster_dingtalk/app_client.py
@@ -18,30 +18,56 @@ class DingTalkClient:
         self.__client_secret: str = client_secret
         self.robot_code: str = client_id
 
-        access_token: str = self._get_access_token()
+        access_token: str = self.__get_access_token()
         self.api: Client = Client(base_url="https://api.dingtalk.com/", headers={"x-acs-dingtalk-access-token": access_token})
         self.oapi: Client = Client(base_url="https://oapi.dingtalk.com/", params={"access_token": access_token})
 
         self.智能人事 = 智能人事_API(self)
         self.通讯录管理 = 通讯录管理_API(self)
         self.文档文件 = 文档文件_API(self)
+        self.互动卡片 = 互动卡片_API(self)
 
-    def _get_access_token(self) -> str:
+    def __get_access_token(self) -> str:
         access_token_cache = Path("/tmp/.dingtalk_cache")
+        all_access_token: dict = {}
+        access_token: str|None = None
+        expire_in: int = 0
+        renew_reason = None
 
-        try:
-            with open(access_token_cache, 'rb') as f:
-                all_access_token: Dict[str, Tuple[str, int]] = pickle.loads(f.read())
-                access_token, expire_in = all_access_token.get(self.app_id, ('', 0))
-        except Exception as e:
-            print(e)
-            all_access_token = {}
-            access_token, expire_in = all_access_token.get(self.app_id, ('', 0))
+        # 从缓存中读取
+        if not access_token_cache.exists():
+            renew_reason = f"鉴权缓存不存在"
+        else:
+            try:
+                with open(access_token_cache, 'rb') as f:
+                    cache_file = f.read()
+            except Exception as e:
+                logging.error(e)
+                cache_file = None
+                renew_reason = "鉴权缓存读取错误"
 
-        if access_token and expire_in < int(time.time()):
+        if cache_file:
+            try:
+                all_access_token = pickle.loads(cache_file)
+            except pickle.PickleError:
+                renew_reason = f"鉴权缓存解析错误"
+
+        if all_access_token:
+            app_access_token = all_access_token.get(self.app_id)
+            access_token = app_access_token.get('access_token')
+            expire_in = app_access_token.get('expire_in')
+        else:
+            renew_reason = f"鉴权缓存不存在该应用 {self.app_name}<{self.app_id}>"
+
+        if not access_token:
+            renew_reason = F"应用 {self.app_name}<{self.app_id}> 的鉴权缓存无效"
+        if expire_in < int(time.time()):
+            renew_reason = F"应用 {self.app_name}<{self.app_id}> 的鉴权缓存过期"
+
+        if renew_reason is None:
             return access_token
         else:
-            logging.warning(f"应用{self.app_name}<{self.app_id}> 鉴权缓存过期或不存在，正在重新获取...")
+            logging.warning(renew_reason)
             response = Client().post(
                 url="https://api.dingtalk.com/v1.0/oauth2/accessToken",
                 json={"appKey": self.client_id, "appSecret": self.__client_secret},
@@ -49,13 +75,17 @@ class DingTalkClient:
             access_token:str = response.json().get("accessToken")
             expire_in:int = response.json().get("expireIn") + int(time.time()) - 60
             with open(access_token_cache, 'wb') as f:
-                all_access_token[self.app_id] = (access_token, expire_in)
+                all_access_token[self.app_id] = {
+                    "access_token": access_token,
+                    "expire_in": expire_in,
+                }
                 f.write(pickle.dumps(all_access_token))
             return access_token
 
 # noinspection NonAsciiCharacters
 class 智能人事_API:
     def __init__(self, _client:DingTalkClient):
+        self.__client:DingTalkClient = _client
         self.花名册 = 智能人事_花名册_API(_client)
         self.员工管理 = 智能人事_员工管理_API(_client)
 
@@ -83,7 +113,6 @@ class 智能人事_花名册_API:
 
 # noinspection NonAsciiCharacters
 class 智能人事_员工管理_API:
-
     def __init__(self, _client:DingTalkClient):
         self.__client:DingTalkClient = _client
 
@@ -125,7 +154,17 @@ class 智能人事_员工管理_API:
 # noinspection NonAsciiCharacters
 class 通讯录管理_API:
     def __init__(self, _client:DingTalkClient):
-        self.__client = _client
+        self.__client:DingTalkClient = _client
+        self.用户管理 = 通讯录管理_用户管理_API(_client)
+        self.部门管理 = 通讯录管理_部门管理_API(_client)
+
+    def 查询用户详情(self, user_id:str, language:str = "zh_CN"):
+        return self.用户管理.查询用户详情(user_id, language)
+
+# noinspection NonAsciiCharacters
+class 通讯录管理_用户管理_API:
+    def __init__(self, _client:DingTalkClient):
+        self.__client:DingTalkClient = _client
 
     def 查询用户详情(self, user_id:str, language:str = "zh_CN") -> dict:
         response = self.__client.oapi.post(url="/topapi/v2/user/get", json={"language": language, "userid": user_id})
@@ -139,16 +178,157 @@ class 通讯录管理_API:
         return response.json()
 
 # noinspection NonAsciiCharacters
+class 通讯录管理_部门管理_API:
+    def __init__(self, _client:DingTalkClient):
+        self.__client:DingTalkClient = _client
+
+    def 获取部门详情(self, dept_id: int, language:str = "zh_CN") -> dict:
+        """
+        调用本接口，根据部门ID获取指定部门详情。
+
+        https://open.dingtalk.com/document/orgapp/query-department-details0-v2
+
+        :param dept_id: 部门 ID ，根部门 ID 为 1。
+        :param language: 通讯录语言。zh_CN en_US
+        """
+        response = self.__client.oapi.post(
+            url="/topapi/v2/department/get",
+            json={"language": language, "dept_id": dept_id}
+        )
+        return response.json()
+
+    def 获取部门列表(self, dept_id: int, language:str = "zh_CN"):
+        """
+        调用本接口，获取下一级部门基础信息。
+
+        https://open.dingtalk.com/document/orgapp/obtain-the-department-list-v2
+
+        :param dept_id: 部门 ID ，根部门 ID 为 1。
+        :param language: 通讯录语言。zh_CN en_US
+        """
+        response = self.__client.oapi.post(
+            url="/topapi/v2/department/listsub",
+            json={"language": language, "dept_id": dept_id}
+        )
+        return response.json()
+
+# noinspection NonAsciiCharacters
 class 文档文件_API:
     def __init__(self, _client:DingTalkClient):
+        self.__client:DingTalkClient = _client
         self.媒体文件 = 文档文件_媒体文件_API(_client)
 
 # noinspection NonAsciiCharacters
 class 文档文件_媒体文件_API:
     def __init__(self, _client:DingTalkClient):
-        self.__client = _client
+        self.__client:DingTalkClient = _client
 
     def 上传媒体文件(self, file_path:Path|str, media_type:Literal['image', 'voice', 'video', 'file']) -> dict:
+        """
+        调用本接口，上传图片、语音媒体资源文件以及普通文件，接口返回媒体资源标识 media_id。
+
+        https://open.dingtalk.com/document/orgapp/upload-media-files
+
+        :param file_path: 本地文件路径
+        :param media_type: 媒体类型，支持 'image', 'voice', 'video', 'file'
+
+        :return:
+            {
+                "errcode": 0,
+                "errmsg": "ok",
+                "media_id": "$iAEKAqDBgTNAk",
+                "created_at": 1605863153573,
+                "type": "image"
+            }
+        """
         with open(file_path, 'rb') as f:
             response = self.__client.oapi.post(url=f"/media/upload?type={media_type}", files={'media': f})
+        return response.json()
+
+# noinspection NonAsciiCharacters
+class 互动卡片_API:
+    def __init__(self, _client:DingTalkClient):
+        self.__client:DingTalkClient = _client
+
+    def 创建并投放卡片(
+            self, search_type_name: str, search_desc: str, card_template_id: str, card_param_map: dict,
+            alert_content: str, open_space_ids: List[str], out_track_id: str, support_forward: bool = True,
+            call_back_type: str = "STREAM", expired_time_millis:int = 0
+    ) -> dict:
+        """
+        创建并投放卡片。当前仅支持 IM群聊, IM机器人单聊, 吊顶 三种场域类型。
+
+        https://open.dingtalk.com/document/orgapp/create-and-deliver-cards
+
+        :param card_template_id: 卡片模板 ID
+        :param open_space_ids: 卡片投放场域 Id
+        :param out_track_id: 卡片唯一标识
+        :param card_param_map: 卡片数据
+        :param search_type_name: 卡片类型名
+        :param search_desc: 卡片消息展示
+        :param alert_content: 通知内容
+        :param call_back_type: 回调模式
+        :param support_forward: 是否支持转发
+        :param expired_time_millis: 吊顶投放过期时间。当投放内容为吊顶时必须传参。
+        """
+
+        open_space_id = f"dtv1.card//{';'.join(open_space_ids)}"
+
+        payload = {
+            "cardTemplateId": card_template_id,
+            "outTrackId": out_track_id,
+            "openSpaceId": open_space_id,
+            "callbackType": call_back_type,
+            "cardData": {"cardParamMap": card_param_map}
+        }
+
+        open_space_model = {
+                "supportForward": support_forward,
+                "searchSupport": {"searchTypeName": search_type_name, "searchDesc": search_desc},
+                "notification": {"alertContent": alert_content, "notificationOff": False}
+            }
+
+        if 'IM_GROUP' in open_space_id.upper():
+            payload["imGroupOpenSpaceModel"] = open_space_model
+            payload["imGroupOpenDeliverModel"] = {"robotCode": self.__client.robot_code}
+
+        if 'IM_ROBOT' in open_space_id.upper():
+            payload["imRobotOpenSpaceModel"] = open_space_model
+            payload["imRobotOpenDeliverModel"] = {"spaceType": "IM_ROBOT", "robotCode": self.__client.robot_code}
+
+        if 'ONE_BOX' in open_space_id.upper():
+            if expired_time_millis == 0:
+                expired_time_millis = int(time.time()+3600)*1000
+            payload["topOpenSpaceModel"] = {"spaceType": "ONE_BOX"}
+            payload["topOpenDeliverModel"] = {"platforms": ["android","ios","win","mac"], "expiredTimeMillis": expired_time_millis,}
+
+        response = self.__client.api.post(
+            url="/v1.0/card/instances/createAndDeliver",
+            json=payload
+        )
+
+        return response.json()
+
+    def 更新卡片(self, out_track_id: str, card_param_map: dict, update_card_data_by_key:bool=True) -> dict:
+        """
+        调用本接口，实现主动更新卡片数据。
+
+        https://open.dingtalk.com/document/orgapp/interactive-card-update-interface
+
+        :param out_track_id: 外部卡片实例Id。
+        :param card_param_map: 卡片模板内容。
+        :param update_card_data_by_key: True-按 key 更新 cardData 数据 False-覆盖更新 cardData 数据
+        :return:
+            {success: bool, result: bool}
+        """
+
+        response = self.__client.api.put(
+            url="/v1.0/card/instances",
+            json={
+                "outTrackId": out_track_id,
+                "cardData": {"cardParamMap": card_param_map},
+                "cardUpdateOptions": {"updateCardDataByKey": update_card_data_by_key}
+            }
+        )
+
         return response.json()
